e186d2b2ecbcffab28c59b9bb4aa0ae9a5df9812
Khim Bahadur Gurung <khimbahadurgurung@khims-mbp.speedport.ip>
2022-08-22 00:30:57 +0200

    axios client update

--- src/axios/index.tsx
+++ src/axios/index.tsx
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 const AxiosClient = axios.create({
   baseURL: process.env.BACKEND_URL,
-  timeout: 1000,
   headers: {
     'Accept': 'application/x-www-form-urlencoded' 
     }
