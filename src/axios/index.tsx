43860b4921c109e720751c5321dd8c6a86897444
Khim Bahadur Gurung <khimbahadurgurung@Khims-MacBook-Pro.local>
2022-08-22 13:25:56 +0200

    chnage base url backend

--- src/axios/index.tsx
+++ src/axios/index.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const AxiosClient = axios.create({
-  baseURL: 'https://localhost:3001/',
+  baseURL: process.env.BACKEND_URL,
   headers: {
     'Accept': 'application/x-www-form-urlencoded' 
     }
